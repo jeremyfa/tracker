724112fcc81613b73a55a789c8df286cf184d881
Jeremy Faivre <contact@jeremyfa.com>
2020-09-23 10:40:46 +0200

    When asking pool for arrays larger than 100K elements, just create one on the fly for one time use

--- src/tracker/ArrayPool.hx
+++ src/tracker/ArrayPool.hx
@@ -18,6 +18,8 @@ class ArrayPool {
 
     static var dynPool100000:ArrayPool = new ArrayPool(100000);
 
+    static var didNotifyLargePool:Bool = false;
+
     public static function pool(size:Int):ArrayPool {
 
         if (size <= 10) {
@@ -36,7 +38,15 @@ class ArrayPool {
             return cast dynPool100000;
         }
         else {
-            return null;
+            if (!didNotifyLargePool) {
+                didNotifyLargePool = true;
+                Tracker.backend.delay(null, 0.5, () -> {
+                    didNotifyLargePool = false;
+                });
+
+                trace('[warning] You should avoid asking a pool for arrays with more than 100000 elements (asked: $size) because it needs allocating a temporary one-time pool each time for that.');
+            }
+            return new ArrayPool(size);
         }
 
     }
